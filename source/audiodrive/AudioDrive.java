566c8bbbfa758b8095385ea521b44925042e831e
Tenjix <tenjix@gmail.com>
2014-08-17 17:19:25 +0200

    removed JavaFX context

--- source/audiodrive/AudioDrive.java
+++ source/audiodrive/AudioDrive.java
@@ -2,44 +2,28 @@ package audiodrive;
 
 import java.awt.Font;
 
-import javafx.application.Application;
-import javafx.application.Platform;
-import javafx.stage.Stage;
 import audiodrive.ui.components.Scene;
 import audiodrive.ui.components.Text;
 import audiodrive.ui.components.Window;
 import audiodrive.ui.scenes.TitleScene;
 import audiodrive.utilities.Log;
 
-public class AudioDrive extends Application {
+public class AudioDrive {
 	
 	public static final String Title = "AudioDrive";
 	public static final Font Font = Text.getFont("Shojumaru");
 	
-	private static Stage stage;
-	
 	public static void main(String[] args) {
-		launch(args);
-	}
-	
-	@Override
-	public void start(Stage stage) throws Exception {
 		Log.info("AudioDrive");
-		AudioDrive.stage = stage;
 		
 		Window.setBorderless(true);
 		Window.setFullscreen(true);
 		Window.setVSyncEnabled(true);
 		Window.open(Scene.get(TitleScene.class));
-		
-		Platform.exit();
 	}
 	
 	public static void exit() {
 		Window.close();
 	}
 	
-	public static Stage getStage() {
-		return stage;
-	}
 }
