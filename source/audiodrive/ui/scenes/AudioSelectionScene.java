2845f925500b35dbfd55ba36cd87b164c3560e56
Tenjix <tenjix@gmail.com>
2014-08-23 16:00:23 +0200

    added menu shortcuts

--- source/audiodrive/ui/scenes/AudioSelectionScene.java
+++ source/audiodrive/ui/scenes/AudioSelectionScene.java
@@ -1,8 +1,6 @@
 package audiodrive.ui.scenes;
 
-import static org.lwjgl.opengl.GL11.GL_COLOR_BUFFER_BIT;
-import static org.lwjgl.opengl.GL11.GL_QUADS;
-import static org.lwjgl.opengl.GL11.glClear;
+import static org.lwjgl.opengl.GL11.*;
 
 import java.io.File;
 import java.util.Arrays;
@@ -37,17 +35,17 @@ import audiodrive.utilities.Log;
  *
  */
 public class AudioSelectionScene extends Scene implements ItemListener {
-
+	
 	private static final String PARENT_FILENAME = "..";
 	private Menu itemMenu;
 	private Menu rootMenu;
 	private Menu nextMenu;
-
+	
 	private MenuItem continueMenuItem;
 	private Map<FileChooserItem, File> itemMap = new HashMap<FileChooserItem, File>();
 	private Map<FileChooserItem, File> rootMap = new HashMap<FileChooserItem, File>();
 	FileSystemView fsv = FileSystemView.getFileSystemView();
-
+	
 	private File selectedFile = null;
 	private Text currentFolderText;
 	private Text selectedFileText;
@@ -60,38 +58,40 @@ public class AudioSelectionScene extends Scene implements ItemListener {
 	private List<String> supportedFileExtensionList = Arrays.asList("mp3", "wav");
 	// Item which was selected
 	private Item item = null;
-
+	
 	public void enter(double duration) {
 		this.duration = duration;
 		super.enter();
 	}
-
+	
 	@Override
 	public void entering() {
 		Camera.overlay(getWidth(), getHeight());
 		canvas = new VertexBuffer(Buffers.create(0, 0, 0, getHeight(), getWidth(), getHeight(), getWidth(), 0), 2).mode(GL_QUADS);
 		shader = new ShaderProgram("shaders/default.vs", "shaders/title.fs");
-
+		
 		titleText = new Text("Choose an AudioFile").setFont(AudioDrive.Font).setSize(48).setPosition(20, 20);
-
+		
 		int textHeight = 50;
-
+		
 		rootMenu = new Menu(20, 200, FileChooserItem.FILECHOOSER_ITEM_WIDTH + 1, Display.getHeight() - 500, 1);
-		itemMenu = new Menu(30 + FileChooserItem.FILECHOOSER_ITEM_WIDTH, 200, Display.getWidth() - FileChooserItem.FILECHOOSER_ITEM_WIDTH - 50, Display.getHeight() - 200 - 3
-				* textHeight, 1);
+		itemMenu = new Menu(30 + FileChooserItem.FILECHOOSER_ITEM_WIDTH, 200, Display.getWidth() - FileChooserItem.FILECHOOSER_ITEM_WIDTH - 50, Display.getHeight()
+			- 200
+			- 3
+			* textHeight, 1);
 		nextMenu = new Menu(20, itemMenu.getHeight() + 200 + 50, 400, Display.getHeight() - itemMenu.getHeight() + 200 + 50, 1);
-
+		
 		continueMenuItem = new MenuItem("Continue", this);
 		continueMenuItem.setDisabled(true);
 		nextMenu.addItem(continueMenuItem);
-
+		
 		currentFolderText = new Text().setFont(AudioDrive.Font).setPosition(20, 150).setSize(30);
 		selectedFileText = new Text().setFont(AudioDrive.Font).setPosition(20, itemMenu.getHeight() + 200).setSize(30);
-
+		
 		// Setup start node
 		File rootFile = new File("./music/");
 		updateItemExplorer(rootFile);
-
+		
 		for (File file : File.listRoots()) {
 			final boolean directory = file.isDirectory();
 			if (!directory) {
@@ -106,12 +106,11 @@ public class AudioSelectionScene extends Scene implements ItemListener {
 		hoverAudio = new AudioFile("sounds/hover.wav");
 		selectAudio = new AudioFile("sounds/select.wav");
 	}
-
+	
 	/**
 	 * Updates the explorer with the given file as root node
 	 * 
-	 * @param rootFile
-	 *            The root file of the tree to be displayed
+	 * @param rootFile The root file of the tree to be displayed
 	 */
 	private void updateItemExplorer(File rootFile) {
 		currentFolderText.setText("Current folder: " + rootFile.getAbsolutePath());
@@ -123,17 +122,17 @@ public class AudioSelectionScene extends Scene implements ItemListener {
 		if (parentFile != null && !fsv.isFileSystemRoot(rootFile)) {
 			FileChooserItem parentFileChooser = new FileChooserItem(PARENT_FILENAME, true, this);
 			itemMenu.addItem(parentFileChooser);
-
+			
 			itemMap.put(parentFileChooser, parentFile);
 		}
 		// Check if there are files available
 		if (listFiles == null) {
 			return;
 		}
-
+		
 		for (File file : listFiles) {
 			final boolean directory = file.isDirectory();
-
+			
 			String fileName = file.getName();
 			String extension = getFileExtension(fileName);
 			// Check if file is supported
@@ -143,14 +142,14 @@ public class AudioSelectionScene extends Scene implements ItemListener {
 				itemMap.put(fci, file);
 			}
 		}
-
+		
 	}
-
+	
 	@Override
 	public void update(double elapsed) {
 		duration += elapsed;
 	}
-
+	
 	@Override
 	public void render() {
 		glClear(GL_COLOR_BUFFER_BIT);
@@ -159,7 +158,7 @@ public class AudioSelectionScene extends Scene implements ItemListener {
 		shader.uniform("resolution").set((float) getWidth(), (float) getHeight());
 		canvas.draw();
 		shader.unbind();
-
+		
 		if (item != null) {
 			checkItemExplorer(itemMap);
 			checkItemExplorer(rootMap);
@@ -172,14 +171,14 @@ public class AudioSelectionScene extends Scene implements ItemListener {
 		selectedFileText.render();
 		titleText.render();
 	}
-
+	
 	private void checkItemExplorer(Map<FileChooserItem, File> map) {
 		final Set<FileChooserItem> keySet = map.keySet();
 		boolean update = false;
 		File rootFile = null;
-
+		
 		for (FileChooserItem fileChooserItem : keySet) {
-
+			
 			if (item == fileChooserItem) {
 				final File file = map.get(fileChooserItem);
 				update = true;
@@ -193,66 +192,71 @@ public class AudioSelectionScene extends Scene implements ItemListener {
 				}
 			}
 		}
-
+		
 		if (update) {
 			updateItemExplorer(rootFile);
 		}
 	}
-
+	
 	@Override
 	public void exiting() {
 		canvas = null;
 		shader = null;
 		selectedFile = null;
 	}
-
+	
 	@Override
 	public void mouseMoved(int x, int y, int dx, int dy) {
 		// yCoordinates start in left bottom corner, instead left top
 		y = getHeight() - y;
-
+		
 		itemMenu.mouseMoved(x, y);
 		rootMenu.mouseMoved(x, y);
 		nextMenu.mouseMoved(x, y);
 	}
-
+	
 	@Override
 	public void mouseButtonReleased(int button, int x, int y) {
 		// yCoordinates start in left bottom corner, instead left top
 		y = getHeight() - y;
-
+		
 		itemMenu.mousePressed(button, x, y);
 		rootMenu.mousePressed(button, x, y);
 		nextMenu.mousePressed(button, x, y);
 	}
-
+	
 	@Override
 	public void keyReleased(int key, char character) {
 		Log.trace("Key '" + character + "' was realeased,");
 		switch (key) {
-
 		case Keyboard.KEY_ESCAPE:
 			exit();
+			break;
+		case Keyboard.KEY_M:
+			Scene.get(ModelViewerScene.class).enter();
+			break;
+		case Keyboard.KEY_RETURN:
+			onSelect(continueMenuItem, true);
 			break;
 		default:
 			break;
 		}
 	}
-
+	
 	@Override
 	public void onHover(Item item, boolean hover) {
 		if (hover) {
 			hoverAudio.play();
 		}
 	}
-
+	
 	@Override
 	public void onSelect(Item item, boolean select) {
 		if (!select) {
 			return;
 		}
 		selectAudio.play();
-
+		
 		if (item == continueMenuItem) {
 			if (selectedFile != null) {
 				Scene.get(AnalyzationScene.class).enter(new AudioFile(selectedFile));
@@ -263,22 +267,21 @@ public class AudioSelectionScene extends Scene implements ItemListener {
 			this.item = item;
 		}
 	}
-
+	
 	/**
 	 * Returns the file extension of the given filename
 	 * 
-	 * @param fileName
-	 *            fileName do get extension from
+	 * @param fileName fileName do get extension from
 	 * @return file extension
 	 */
 	private String getFileExtension(String fileName) {
 		String extension = "";
-
+		
 		int i = fileName.lastIndexOf('.');
 		if (i > 0) {
 			extension = fileName.substring(i + 1);
 		}
-
+		
 		return extension;
 	}
 }
