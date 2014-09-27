e0451805fbcc87828527ef5aefff847ea03de477
Tenjix <tenjix@gmail.com>
2014-09-27 13:27:09 +0200

    improved and documented versioning

--- source/audiodrive/utilities/Versioning.java
+++ source/audiodrive/utilities/Versioning.java
@@ -8,31 +8,58 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
-import audiodrive.Resources;
-
+/**
+ * <b>Provides automatic versioning when using a git repository with tags.</b><br>
+ * <br>
+ * The version will be updated when {@linkplain #getVersion()} is called by the program from within the git repository's working directory. This requires either a stand-alone
+ * <b>git</b> version or <b>SourceTree</b> with embedded git.<br>
+ * <br>
+ * The version string will be obtained using "<i>git describe</i>" and therefore consist of: <br>
+ * "<i>{latest tag}</i>" if the latest commit is tagged, or <br>
+ * "<i>{latest tag}-[number of commits since latest tag}-g[latest commit hash}</i>" if there are commits after the latest tag.<br>
+ * <br>
+ * After obtaining the version it will be stored with a "<i>version</i>" file inside the source directory. This file should then be included into the executable jar, from where it
+ * will be read once on every program start.<br>
+ * <br>
+ * If there are no tags present in the git repository, the version will be "<i>Unspecified</i>" and no version file will be created.
+ */
 public class Versioning {
 	
-	private static String version = "Unspecified";
+	private static final String Unspecified = "Unspecified";
+	
+	private static String version;
 	
 	/** Private constructor to prevent instantiation. */
 	private Versioning() {
 		throw new IllegalStateException("This class shall not be instantiated.");
 	}
 	
+	/**
+	 * Returns the current version as returned by the git repository using "<i>git describe</i>".
+	 * 
+	 * @return the current program version
+	 * @see Versioning
+	 */
 	public static String getVersion() {
 		if (version != null) return version;
-		if (new File(".git").exists()) {
-			updateVersion();
-			store();
+		if (gitDirectory().exists()) {
+			update();
+			write();
 		} else {
 			read();
+		}
+		if (version == null) {
+			version = Unspecified;
+			versionFile().delete();
 		}
 		return version;
 	}
 	
-	private static void store() {
-		try (FileWriter writer = new FileWriter(new File("source/version"))) {
+	private static void write() {
+		if (version == null) return;
+		try (FileWriter writer = new FileWriter(versionFile())) {
 			writer.write(version);
 		} catch (IOException exception) {
 			Log.warning("Couldn't create version file.");
@@ -40,7 +67,7 @@ public class Versioning {
 	}
 	
 	private static void read() {
-		InputStream stream = Resources.stream("version");
+		InputStream stream = ClassLoader.getSystemResourceAsStream("version");
 		if (stream != null) try (BufferedReader reader = new BufferedReader(new InputStreamReader(stream))) {
 			version = reader.readLine();
 			return;
@@ -48,7 +75,7 @@ public class Versioning {
 		Log.warning("Couldn't find version file.");
 	}
 	
-	public static void updateVersion() {
+	private static void update() {
 		String command = "git.exe describe";
 		try {
 			version = execute(command);
@@ -72,6 +99,19 @@ public class Versioning {
 		} catch (IOException exception) {
 			throw new RuntimeException(exception);
 		}
+	}
+	
+	private static File gitDirectory() {
+		return new File(".git");
+	}
+	
+	private static File versionFile() {
+		List<String> possibilities = Arrays.asList("source", "src");
+		Optional<File> sourceDirectory = possibilities.stream().map(File::new).filter(File::exists).findAny();
+		if (!sourceDirectory.isPresent()) {
+			throw new RuntimeException("Couldn't find source directory.");
+		}
+		return new File(sourceDirectory.get().getName() + File.separator + "version");
 	}
 	
 }
