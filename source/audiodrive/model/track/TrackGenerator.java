cce6f714f30aac1ee1d77b95f8746ad2fd8f38a5
Tenjix <tenjix@gmail.com>
2015-12-07 20:45:16 +0100

    improved block placement

--- source/audiodrive/model/track/TrackGenerator.java
+++ source/audiodrive/model/track/TrackGenerator.java
@@ -60,21 +60,23 @@ public class TrackGenerator {
 		int minumumDistance = (int) Math.round(iterationRate * 0.2);
 		for (int iteration = offset; iteration < spline.size() - offset; iteration++) {
 			double intensity = mixed.getSpectralSum().getClamped(iteration);
-			double calmness = 1 - intensity;
+			double calmness = 1.0 - intensity;
 			double leftFlux = left.getSpectralFlux().getClamped(iteration);
 			double rightFlux = right.getSpectralFlux().getClamped(iteration);
 			double leftPeak = left.getPeaks().getClamped(iteration);
 			double rightPeak = right.getPeaks().getClamped(iteration);
-			double threshold = 0.25 + calmness * 0.2; // calm music -> fewer blocks
-			if (leftFlux + leftPeak < threshold && rightFlux + rightPeak < threshold) continue;
-			int rail = (int) Math.signum(Arithmetic.significance(leftFlux - rightFlux, 0.05));
+			double leftIntensity = leftFlux + leftPeak;
+			double rightIntensity = rightFlux + rightPeak;
+			double threshold = 0.15 + calmness * 0.3; // calm music -> fewer blocks
+			if (leftIntensity < threshold && rightIntensity < threshold) continue;
+			int rail = (int) Math.signum(Arithmetic.significance(leftIntensity - rightIntensity, 0.05));
 			int r = rail + 1;
 			if (previous[r] != null && iteration - previous[r].iteration() < minumumDistance) continue;
-			threshold = 0.4 + calmness * 0.2; // intense music -> more collectables, fewer obstacles
+			threshold = 0.5 + intensity * 0.1; // intense music -> more obstacles, fewer collectables
 			int collidables = 0;
 			for (int i = 0; i < previous.length; i++)
 				if (i != r && previous[i] != null && !previous[i].isCollectable()) collidables++;
-			boolean collectable = collidables == 2 ? true : (leftFlux > threshold || rightFlux > threshold);
+			boolean collectable = collidables == 2 ? true : (leftIntensity > threshold || rightIntensity > threshold);
 			blocks.add(previous[r] = new Block(collectable, iteration, rail));
 		}
 		Log.debug(blocks.size() + " blocks");
